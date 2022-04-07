ef09382643b1cba293d0d63ca3fb9ffd59b0268f
dhruvang-uplers <dhruvang.g@uplers.in>
2022-04-07 14:41:25 +0530

    setup

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import 'dotenv/config'
+
+import express from 'express'
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.json('Hello World!')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
